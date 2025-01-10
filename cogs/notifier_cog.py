1e5c2458bba23c4a5e10faafac0c584c689f1c62
theowiik <theo.le.wiik@gmail.com>
2025-01-11 00:41:45 +0100

    Make AT_TARGET optional

--- cogs/notifier_cog.py
+++ cogs/notifier_cog.py
@@ -6,7 +6,7 @@ from discord.ext.commands import Cog
 class NotifierCog(Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.target_user_id = int(os.getenv("AT_TARGET"))
+        self.target_user_id = int(os.getenv("AT_TARGET", 0))
 
     @Cog.listener()
     async def on_message(self, message):
